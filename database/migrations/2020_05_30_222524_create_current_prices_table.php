bca16c64525a4b0e8b9c01e04d6910c76eaef32c
Rober Song <jack90908@gmail.com>
2020-06-01 18:30:15 +0800

    調整config及新增資料庫

---
+++ database/migrations/2020_05_30_222524_create_current_prices_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCurrentPricesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('current_prices', function (Blueprint $table) {
+            $table->bigInteger('date')->primary();
+            $table->decimal('amount', 5, 2);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('current_prices');
+    }
+}
